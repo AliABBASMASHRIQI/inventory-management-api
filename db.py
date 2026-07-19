7bb06c0679f193810c4a4c300a1def78f29693a2
ALI ABBAS MASHRIQI <aliabbasmashriqi@gmail.com>
2026-07-19 14:24:06 +0530

    Add db changes

--- db.py
+++ db.py
@@ -1,12 +1,14 @@
-from config import host,password,user,database
+from config import host, user, password, database, port
 import mysql.connector
 
 connection = mysql.connector.connect(
-    host = host,
-    user = user,
-    password = password,
-    database = database
+    host=host,
+    port=port,
+    user=user,
+    password=password,
+    database=database
 )
+
 print(connection.is_connected())
 
 cursor = connection.cursor()
